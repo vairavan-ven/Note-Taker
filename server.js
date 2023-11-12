2675ead2ee4f6fb2ec0571a348517be4d2b998d2
vairavan-ven <vairavan.ven@gmail.com>
2023-11-12 22:36:08 +1100

    created get routes for html

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define routes for serving HTML files
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
