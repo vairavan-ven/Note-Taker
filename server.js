bf74304ddcff26597147069d1ebee952c69e09e3
vairavan-ven <vairavan.ven@gmail.com>
2023-11-13 13:57:02 +1100

    made changes to server

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid  = require('uuid'); // Use the v4 method from the uuid package
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define routes for serving HTML files
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Define routes for serving HTML files
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
