63fcb9050002027b6a31ad24e9e6ee21b0b31c0c
vairavan-ven <vairavan.ven@gmail.com>
2023-11-13 14:22:18 +1100

    connected to the server

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
