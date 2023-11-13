78d77f1eca5f81c3a595d38407b91d07a76c1309
vairavan-ven <vairavan.ven@gmail.com>
2023-11-13 14:09:47 +1100

    made an edit to the index get route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
